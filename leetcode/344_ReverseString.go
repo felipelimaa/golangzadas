f50728a68d04616f376190cb9c0917b9b1a7b375
Felipe Lima <fearlimasi@gmail.com>
2024-04-26 23:03:18 -0300

    feat: leatcode 344 reverse string

---
+++ leetcode/344_ReverseString.go
@@ -0,0 +1,16 @@
+package leetcode
+
+import "fmt"
+
+// Write a function that reverses string. The input string is given as an array of characteres s.
+// You must do this by modifying the input array in-place with 0(1) extra memory.
+func ReverseString(s []string) {
+	l, r := 0, len(s)-1
+	for l < r {
+		s[l], s[r] = s[r], s[l]
+		l += 1
+		r -= 1
+	}
+
+	fmt.Println(s)
+}
--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"golangzadas/leetcode"
 )
 
 func main() {
 	fmt.Println("Welcome to golangzadas")
+
+	var s = []string{"h", "e", "l", "l", "o"}
+	leetcode.ReverseString(s)
 }
